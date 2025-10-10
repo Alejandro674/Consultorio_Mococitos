620009dd55d8371131dfd093c17563723c16afb0
Jonathan Ruiz Rodriguez <jruizr017@alumno.uaemex.mx>
2025-10-09 23:23:37 -0600

    Ṕrimer commit front

---
+++ src/views/general/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <body>
+    <ApplicationRoot />
+  </body>
+</template>
+
+<script setup lang="ts">
+  import ApplicationRoot from "@/components/app/ApplicationRoot.vue"
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/general/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card flex justify-center">
+    <div class="flex flex-col gap-1">
+      <InputText name="username" type="text" placeholder="Username" v-model="cred.username" />
+    </div>
+    <div class="flex flex-col gap-1">
+      <InputText name="password" type="password" placeholder="Password" v-model="cred.password" />
+    </div>
+    <Button type="submit" severity="success" label="Iniciar Sesion" @click="security.login( cred )" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { securityStore } from "@/addons/store/general/security.ts";
+import {ref} from "vue";
+
+const security = securityStore();
+
+const cred = ref({
+  username: '',
+  password: ''
+});
+
+</script>
